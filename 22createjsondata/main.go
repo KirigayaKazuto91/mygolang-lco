618cb969526682b6283e99f56bf1349fc52a1aba
KirigayaKazuto91 <mike_shadowz91@protonmail.com>
2024-04-14 00:50:13 +0700

    learn decode json

--- 22createjsondata/main.go
+++ 22createjsondata/main.go
@@ -16,7 +16,8 @@ type course struct {
 func main() {
 	fmt.Println("Welcome to Json Data Creation in Golang!")
 
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson(){
@@ -33,5 +34,39 @@ func EncodeJson(){
 		panic(err)
 	}
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodeJson(){
+	jsonDataFromWeb := []byte(`
+		{
+			"coursename": "ReactJS Bootcamp",
+			"price": 299,
+			"website": "LearnCodeOnline.in",
+			"tags": ["web-dev", "js", "react"]
+		}
+	`)
+
+	var lcoCourse course
+	
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("Course: %+v\n", lcoCourse)
+
+	} else {
+		fmt.Println("JSON was not valid")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("Data: %#v\n", myOnlineData) 
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is: %v and value is: %v and type is %T\n", k, v, v)
+	}
+	
 }
 
